970494d4af068b63966f226e26b227bb33b5f668
Flavius <flaviusbejan96@yahoo.com>
2022-09-15 15:33:15 +0300

    Finish

---
+++ exercise.js
@@ -0,0 +1,12 @@
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+yearsCompleted == firstYearCompleted;
+yearsCompleted === firstYearCompleted;
+
+/* A double equal sign is used for comparing two
+ variable by values only.
+  A triple egual sign is used for comparing two
+   variables by both values and data types.
+
+*/
